90a618fad931b42f89fd5c3b2d92c8bcaeb14ea3
aderesie <aderesie.m.s@gmail.com>
2020-04-28 22:28:01 +0700

    Edit Result.kt

--- app/src/main/java/lab/chevalier/moviedb/api/response/Result.kt
+++ app/src/main/java/lab/chevalier/moviedb/api/response/Result.kt
@@ -28,8 +28,6 @@ data class Result(
     val popularity: Double = 0.0,
     @Json(name = "poster_path")
     val posterPath: String = "",
-    @Json(name = "release_date")
-    val releaseDate: String = "",
     @Json(name = "title")
     val title: String = "",
     @Json(name = "video")
